8cc61665d973e2b821b9baec2881a460d00f7ddd
Maayan <maayanba24@gmail.com>
2024-02-21 23:29:58 +0200

    alpha functionallity done

--- commands/displayQueue.js
+++ commands/displayQueue.js
@@ -11,8 +11,7 @@ module.exports = {
     const songList = [];
     let index = 1;
     for (const song in queue) {
-      // if (playlist.current == null && song == 0) { continue; }
-      const newSong = { name: `${index}) priority: ${queue[song].priority}`, value: `${queue[song].title}`, inline: false };
+      const newSong = { name: `${index}) added by: ${queue[song].requestedBy}`, value: `${queue[song].title}`, inline: false };
       index += 1;
       songList.push(newSong);
     }
@@ -25,14 +24,19 @@ module.exports = {
       interaction.reply({ embeds: [embed] });
       return;
     }
-    embed.setTitle('currently playing');
     embed.setColor('#9747FF');
     embed.addFields(songList);
     embed.setTimestamp();
     if (playlist.current != null) {
+      if (playlist.player.playing) {
+        embed.setTitle('Currently playing:');
+      } else {
+        embed.setTitle('Currently paused:');
+      }
       embed.setDescription(playlist.current.title);
       embed.setThumbnail(playlist.current.thumbnail.thumbnails[0].url);
     } else {
+      embed.setTitle('Playing next:');
       embed.setDescription(queue[0].title);
       embed.setThumbnail(queue[0].thumbnail.thumbnails[0].url);
     }
