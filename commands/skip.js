48fd9fccf755945c1b8964ba2da1fae7310a5721
ronyzrihen <ronyzn3@gmail.com>
2024-02-23 01:28:08 +0200

    added some cosmetics to skip function and added embed reply for cases when trying to skip the last song of the queue

--- commands/skip.js
+++ commands/skip.js
@@ -6,14 +6,19 @@ module.exports = {
     .setName('skip')
     .setDescription('skip the current song.'),
   execute: async ({ interaction, playlist }) => {
-    if (!playlist.queue) {
-      await interaction.reply('there is no song playlist.');
+    if (!playlist.queue.size()) {
+      const embed = new EmbedBuilder(); // FIXME when skiping last song the music wont stop playing
+      embed.setTitle('Ops...');
+      embed.setDescription('***Queue is empty***');
+      embed.setColor('#ff0000');
+      await interaction.reply({ embeds: [embed] });
       return;
     }
     playlist.skip();
     const embed = new EmbedBuilder();
     embed.setTitle('skipped');
     embed.setDescription(`now playing **${playlist.current.title}**`);
+    embed.setColor('#9747FF');
     // embed.setThunbnail(playlist.current.thumbnail);
     interaction.reply({ embeds: [embed] });
   },
